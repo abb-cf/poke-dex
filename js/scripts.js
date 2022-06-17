7fac17654ec94d9d9e9882ceb01706ca272a92f5
abb-cf <106682621+abb-cf@users.noreply.github.com>
2022-06-17 11:04:27 -0400

    adds pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let pokemonList = [
+{
+  name: "Bulbasaur",
+  height: 7,
+  types: "poison"
+},
+{
+  name: "Charmander",
+  height: 6,
+  types: "fire",
+},
+{
+  name: "Squirtle",
+  height: 5,
+  types: "water",
+}
+];
+
+/*
+console.log(pokemonList[0]);
+console.log(pokemonList[1]);
+console.log(pokemonList[2]);
+*/
