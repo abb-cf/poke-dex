03a8c3451fd3378dde2bd2f17e26e2987b5969a3
abb-cf <106682621+abb-cf@users.noreply.github.com>
2022-06-23 14:49:41 -0400

    updates for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,11 @@ console.log(pokemonList[2]);
 */
 
 //displays each pokemon in array along with their height
-//adds exclamation with if/else loop if pokemon height is greater than 6.
-for (let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' );
-  if (pokemonList[i].height > 6) {
-      document.write(' - Wow! That\'s big!' + '<br>');
-  } else {
-    document.write('<br>')
+//adds exclamation with if loop if pokemon height is greater than 6.
+
+pokemonList.forEach(function(pokemon) {
+  document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') ');
+  if (pokemon.height > 6) {
+    document.write(' - Wow! That\'s big!');
   }
-}
+});
